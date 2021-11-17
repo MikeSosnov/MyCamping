9d1975e7754cbd04a1f0c8f7f9c79f48694d9eb8
Nikita <sosnonikita@gmail.com>
2021-11-17 15:45:11 +0300

    new

--- app/src/main/java/com/example/mycamping/MainActivity.kt
+++ app/src/main/java/com/example/mycamping/MainActivity.kt
@@ -4,17 +4,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-        lateinit var button : Button
+        private lateinit var button : Button
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.button)
     }
 
-    fun Good(view: android.view.View) {
+    fun My_tag_Log() {
         Log.d("My Tag", "Hello World")
     }
 }
